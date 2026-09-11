6eb6fe797f6c3c83f00ab4e403b135a0c5031b89
MrMozzarella-d <timhartmeier@gmail.com>
2026-09-12 01:00:59 +0200

    feat: first version of lexer

--- lexer.cpp
+++ lexer.cpp
@@ -1,1 +1,150 @@
 #include "lexer.h"
+
+#include <cctype>
+
+Lexer::Lexer(std::string_view source)
+:   m_current(0),
+    m_line(0),
+    m_col(0),
+    m_source(source)
+{}
+
+std::vector<Token> Lexer::tokenize() {
+    std::vector<Token> tokens;
+
+    while (!isAtEnd()) {
+        uint64_t start = m_current;
+
+        auto curr = advance();
+        if (!curr.has_value()) break;
+
+        auto next = peek();
+        if (next.has_value()) {
+            auto token = matchDouble(*curr, *next, start);
+            if (token.has_value()) {
+                tokens.emplace_back(*token);
+                continue;
+            }
+        }
+        auto token = matchSingle(*curr, start);
+        if (token.has_value()) {
+            tokens.emplace_back(*token);
+            continue;
+        }
+        if ( std::isdigit(*curr) ) {
+            while (!isAtEnd()) {
+                auto next = advance();
+                if (
+                    !next.has_value()
+                    or !std::isdigit(*next)
+                    ) break;
+            }
+            Span span = { start, m_current };
+            Token token = { Number, span };
+            tokens.push_back(token);
+            continue;
+        }
+        if ( std::isalpha(*curr) ) {
+            while (!isAtEnd()) {
+                auto next = advance();
+                if (!next.has_value()
+                    or !std::isalpha(*next)
+                    ) break;
+            }
+            Span span = { start, m_current };
+            std::string_view str = m_source.substr(start, m_current);
+            TokenKind kind = [str, this]() -> TokenKind {
+                if (m_keywords.contains(str)) {
+                    return m_keywords[str];
+                }
+                return Identifier;
+            }();
+            Token token = { kind, span };
+            tokens.push_back(token);
+        }
+        // TODO: Error handling here
+    }
+    return tokens;
+}
+
+std::optional<Token> Lexer::matchSingle(char curr, uint64_t start) {
+    auto kind = [curr]() -> std::optional<TokenKind> {
+        switch (curr) {
+            case '=': return Assign;
+            case '+': return Add;
+            case '-': return Sub;
+            case '*': return Mul;
+            case '/': return Div;
+
+            case ':': return Colon;
+            case ';': return Semicolon;
+
+            case '{': return OpenBody;
+            case '}': return CloseBody;
+
+            case '(': return OpenParen;
+            case ')': return CloseParen;
+
+            default: return std::nullopt;
+        }
+    }();
+    if (kind.has_value()) {
+        return Token{ *kind, {start, m_current} };
+    }
+    return std::nullopt;
+}
+
+constexpr uint16_t pack2(char c1, char c2) {
+    return static_cast<uint16_t>(c1) << 8 | c2;
+}
+
+std::optional<Token> Lexer::matchDouble(char curr, char next, uint64_t start) {
+    auto kind = [curr, next]() -> std::optional<TokenKind> {
+        uint16_t comb = pack2(curr, next);
+        switch (comb) {
+            case pack2('=', '='): return Equal;
+
+            case pack2('+', '='): return AddAssign;
+            case pack2('+', '+'): return Increment;
+
+            case pack2('-', '='): return SubAssign;
+            case pack2('-', '-'): return Decrement;
+
+            case pack2('*', '='): return MulAssign;
+            case pack2('/', '='): return DivAssign;
+
+            default: return std::nullopt;
+        }
+    }();
+    if (kind.has_value()) {
+        advance();
+        return Token{ *kind, {start, m_current} };
+    }
+    return std::nullopt;
+}
+
+std::optional<char> Lexer::advance() {
+    if (isAtEnd()) return std::nullopt;
+    char c = m_source[m_current];
+    m_current++;
+    m_col++;
+    if (c == '\n') {
+        m_line++;
+        m_col = 0;
+    }
+    return c;
+}
+
+std::optional<char> Lexer::next() {
+    if (m_current + 1 >= m_source.size()) return std::nullopt;
+    return m_source[m_current + 1];
+}
+
+std::optional<char> Lexer::peek() {
+    if (isAtEnd()) return std::nullopt;
+    return m_source[m_current];
+}
+
+bool Lexer::isAtEnd() {
+    return m_current >= m_source.size();
+}
--- lexer.h
+++ lexer.h
@@ -1,27 +1,34 @@
 #ifndef MISH_LEXER_H
 #define MISH_LEXER_H
 #include <cstdint>
+#include <optional>
 #include <string_view>
+#include <unordered_map>
+#include <vector>
 
 enum TokenKind {
     // 0-9
     Number,
     // <a-0-9-z>, <A-0-9-Z>
     Identifier,
+    KwLet,
+    KwFn,
     // =
     Assign,
+    // ==
+    Equal,
     // +
     Add,
     // +=
     AddAssign,
     // ++
-    Adds,
+    Increment,
     // -
     Sub,
     // -=
     SubAssign,
     // --
-    Subs,
+    Decrement,
     // *
     Mul,
     // *=
@@ -59,13 +66,27 @@ struct Token {
 
 class Lexer {
 public:
-    Lexer(std::string_view source);
+    explicit Lexer(std::string_view source);
+
+    std::vector<Token> tokenize();
 private:
     uint64_t m_current;
     uint64_t m_line;
     uint64_t m_col;
     std::string_view m_source;
-};
 
+    std::optional<Token> matchSingle(char curr, uint64_t start);
+    std::optional<Token> matchDouble(char curr, char next, uint64_t start);
+
+    std::optional<char> advance();
+    std::optional<char> next();
+    std::optional<char> peek();
+    bool isAtEnd();
+
+    std::unordered_map<std::string_view, TokenKind> m_keywords = {
+        { "let", KwLet },
+        { "fn", KwFn },
+    };
+};
 
 #endif //MISH_LEXER_H
